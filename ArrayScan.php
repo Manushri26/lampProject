b8ef25195790151d8a33d5f7df3ff4b073fe336b
unknown <manushrijain26@gmail.com>
2021-02-25 11:41:36 +0530

    init

---
+++ ArrayScan.php
@@ -0,0 +1,32 @@
+
+
+<html>
+<head><title>Array Key-Value</title></head>
+
+<form method='post'>
+<pre>
+Enter keys whose value is to be found :<input type='text' name='str1'><br>                                           
+        <input type=submit value= 0k ><input type=reset value=cancel></h2>
+</pre>
+</form>
+<?php
+		
+		if(isset($_POST['str1']))
+		{
+			$str1=$_POST['str1'];
+			$arr1=array("c1"=>"red", "c2"=>"green", "c3"=>"white", "c4"=>"black");
+			$arr=explode(",",$str1);
+			for ($x = 0; $x < sizeof($arr); $x++) {
+				if(array_key_exists($arr[$x],$arr1))
+				{
+					echo "The value for ".$arr[$x]." is:". $arr1[$arr[$x]]." <br>";
+				}
+				else
+				{
+					echo "key ".$arr[$x]." is not valid. <br>";
+				}
+			}
+		}
+?>
+</body>
+</html>
