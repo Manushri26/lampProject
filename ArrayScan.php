c386938d9c76daa7346463bc1977f00bf7137f4a
Mansi <mansigpalsana18@gmail.com>
2021-02-25 13:27:59 +0530

    hello

--- ArrayScan.php
+++ ArrayScan.php
@@ -25,7 +25,7 @@ Enter keys whose value is to be found :<input type='text' name='str1'><br>
 				else
 				{
 					echo "key ".$arr[$x]." is not valid and not applicable. <br>";
-					echo "hellllllllo world." <br>";
+					echo "helllllllllllo world." <br>";
 				}
 			}
 		}
